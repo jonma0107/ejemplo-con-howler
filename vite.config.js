179f7744c7bc5c1170efe40bb470cd244125bc3e
Jonathan Meza <jduplantier44@gmail.com>
2022-06-28 00:31:13 -0500

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ const base = mode === "production" ? "/" + path.basename(process.cwd()) + "/" : 
 
 module.exports = {
   root: "src",
-  base,
+  base: "https://github.com/jonma0107/howler/",
   mode,
   publicDir: "../public",
   build: {
